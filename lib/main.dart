755f9d3714504d21df44d022a0f5ae78ab97a34a
Aliv1380 <Aliv.vtk@gmail.com>
2022-02-21 01:00:28 +0300

    Добавлена панель Navigtion Drwer

--- lib/main.dart
+++ lib/main.dart
@@ -4,13 +4,15 @@ import 'package:flutter/material.dart';
 import 'calc_screen.dart';
 import 'example_radio_switch.dart';
 import 'first_screen.dart';
+import 'navidation_drawer_screen.dart';
 import 'registration_screen.dart';
 
 //RegScreen
 //MyApp
 //CalcScreen
 //ExRadioSwitch
+//NavigstionDrawerScreen
 
 void main() {
-  runApp(const CalcScreen());
+  runApp(const NavigstionDrawerScreen());
 }
